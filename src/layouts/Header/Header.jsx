928158ca3403c089e88273bb9c52d2df6db06e02
matysikgeorge <matysikgeorge1995@gmail.com>
2024-09-09 10:45:28 +0300

    react-gh-pages2

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = ({cardFinder}) => {
     <>
       <header className={s.header}>
         <div className={s.container}>
-          <h1 className={s.header__title}>Emoji Finder</h1>
+          <h1 className={s.header__title}>Emoji Finder!</h1>
           <p className={s.header__text}>Find emoji by keywords</p>
           <Input cardFinder={cardFinder} />
         </div>
